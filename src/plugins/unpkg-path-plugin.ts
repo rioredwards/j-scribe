8b1c622804afa06ef64d9c8212e203532580e40f
Rio Edwards <rioredwards@gmail.com>
2023-03-21 16:55:21 -0700

    Handle nested directories in esbuild plugin

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -14,7 +14,10 @@ export const unpkgPathPlugin = () => {
         if (args.path.includes("./") || args.path.includes("../")) {
           return {
             namespace: "a",
-            path: new URL(args.path, args.importer + "/").href,
+            path: new URL(
+              args.path,
+              "https://unpkg.com" + args.resolveDir + "/"
+            ).href,
           };
         }
 
@@ -31,18 +34,18 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              const message = require('medium-test-pkg');
+              const message = require('nested-test-pkg');
               console.log(message);
             `,
           };
-        } else {
-          const { data } = await axios.get(args.path);
-          console.log("data: ", data);
-          return {
-            loader: "jsx",
-            contents: data,
-          };
         }
+
+        const { data, request } = await axios.get(args.path);
+        return {
+          loader: "jsx",
+          contents: data,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
       });
     },
   };
