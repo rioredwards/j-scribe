7b9870abaa17da8a38216a5be7ddcf934623df68
Rio Edwards <rioredwards@gmail.com>
2023-03-21 20:33:44 -0700

    Add a caching layer using localforage. This allows libraries to be stored in localstorage instead of being fetched over and over

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,17 @@
 import * as esbuild from "esbuild-wasm";
 import axios from "axios";
+import localForage from "localforage";
+
+const fileCache = localForage.createInstance({
+  name: "filecache ",
+});
+
+(async () => {
+  await fileCache.setItem("color", "red");
+
+  const color = await fileCache.getItem("color");
+  console.log(color);
+})();
 
 export const unpkgPathPlugin = () => {
   return {
@@ -34,19 +46,36 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              const react = require('react');
-              const dom = require('react-dom');
+              import react from 'react@16.0.0';
+            
               console.log(react. dom);
             `,
           };
         }
 
+        // Check to see if we have already fetched this file
+        // and if it is in the cache
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+
+        // if it is, return it immediately
+        if (cachedResult) {
+          return cachedResult;
+        }
+
         const { data, request } = await axios.get(args.path);
-        return {
+
+        const result: esbuild.OnLoadResult = {
           loader: "jsx",
           contents: data,
           resolveDir: new URL("./", request.responseURL).pathname,
         };
+
+        // store response in cache
+        await fileCache.setItem(args.path, result);
+
+        return result;
       });
     },
   };
