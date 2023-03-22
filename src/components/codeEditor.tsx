d559ecbe748f03fe6bc15999f024b74f2b80543a
Rio Edwards <rioredwards@gmail.com>
2023-03-22 16:11:18 -0700

    Configure options for MonacoEditor

--- src/components/codeEditor.tsx
+++ src/components/codeEditor.tsx
@@ -4,7 +4,23 @@ import "./codeEditor.css";
 import MonacoEditor from "@monaco-editor/react";
 
 const CodeEditor = () => {
-  return <MonacoEditor className="MonacoEditor" />;
+  return (
+    <MonacoEditor
+      language="javascript"
+      theme="dark"
+      className="MonacoEditor"
+      options={{
+        wordWrap: "on",
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }}
+    />
+  );
 };
 
 export default CodeEditor;
