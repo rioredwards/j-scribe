851c1ea491ec003cb48168a42b802090fa9ad842
Rio Edwards <rioredwards@gmail.com>
2023-03-23 11:15:59 -0700

    Set text-editor edit/view state based on user clicking inside/outside of component

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,11 +1,41 @@
 import MDEditor from "@uiw/react-md-editor";
+import { useState, useEffect, useRef } from "react";
 
 interface Props {}
 
 const TextEditor: React.FC<Props> = (props: Props) => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [editing, setEditing] = useState(false);
+
+  useEffect(() => {
+    // This sets edit mode to false when the user clicks outside the editor
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+      setEditing(false);
+    };
+    document.addEventListener("click", listener, { capture: true });
+    return () => {
+      document.removeEventListener("click", listener, { capture: true });
+    };
+  }, []);
+
+  if (editing) {
+    return (
+      <div ref={ref}>
+        <MDEditor />
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <MDEditor />
+    <div onClick={() => setEditing(true)}>
+      <MDEditor.Markdown source={"# Header"} />
     </div>
   );
 };
