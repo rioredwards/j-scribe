9c09d65e4efc535105c465f248bcd279c5de1fec
Rio Edwards <rioredwards@gmail.com>
2023-03-23 12:21:06 -0700

    Create some interfaces for all the possible actions we may want

---
+++ src/state/actions/index.ts
@@ -0,0 +1,56 @@
+import { ActionType } from "../action-types";
+
+export interface MoveCellAction {
+  type: ActionType.MOVE_CELL;
+  payload: {
+    id: string;
+    direction: "up" | "down";
+  };
+}
+
+export interface DeleteCellAction {
+  type: ActionType.DELETE_CELL;
+  payload: string;
+}
+
+export interface InsertCellBeforeAction {
+  type: ActionType.INSERT_CELL_BEFORE;
+  payload: {
+    id: string;
+    type: "code" | "text";
+  };
+}
+
+export interface UpdateCellAction {
+  type: ActionType.UPDATE_CELL;
+  payload: {
+    id: string;
+    content: string;
+  };
+}
+
+export interface BundleStartAction {
+  type: ActionType.BUNDLE_START;
+  payload: {
+    cellId: string;
+  };
+}
+
+export interface BundleCompleteAction {
+  type: ActionType.BUNDLE_COMPLETE;
+  payload: {
+    cellId: string;
+    bundle: {
+      code: string;
+      err: string;
+    };
+  };
+}
+
+export type Action =
+  | MoveCellAction
+  | DeleteCellAction
+  | InsertCellBeforeAction
+  | UpdateCellAction
+  | BundleStartAction
+  | BundleCompleteAction;
