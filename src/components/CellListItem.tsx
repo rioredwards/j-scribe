3c35b1bfadc142377db44c8f5c7ae9efd3ca3cc4
Rio Edwards <rioredwards@gmail.com>
2023-03-23 19:21:18 -0700

    Render CodeCell and TextEditor components within cellListItem component

--- src/components/CellListItem.tsx
+++ src/components/CellListItem.tsx
@@ -1,11 +1,20 @@
 import { Cell } from "../state";
+import CodeCell from "./CodeCell";
+import TextEditor from "./TextEditor";
 
 interface Props {
   cell: Cell;
 }
 
 const CellListItem: React.FC<Props> = ({ cell }) => {
-  return <div>{cell.id}</div>;
+  let child: JSX.Element;
+  if (cell.type === "code") {
+    child = <CodeCell />;
+  } else {
+    child = <TextEditor />;
+  }
+
+  return <div>{child}</div>;
 };
 
 export default CellListItem;
