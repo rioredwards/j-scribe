bddb453ba93cdb8bdf358959f8e895fbd8a293c1
Rio Edwards <rioredwards@gmail.com>
2023-03-23 18:23:28 -0700

    Re-add state in reducer's returns (not necessary bc/ of immer, but keeps Typescript happy)

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -25,23 +25,23 @@ const cellsReducer = produce(
       case ActionType.UPDATE_CELL:
         const { id, content } = action.payload;
         state.data[id].content = content;
-        return;
+        return state;
       case ActionType.DELETE_CELL:
         delete state.data[action.payload];
         state.order = state.order.filter((id) => id !== action.payload);
-        return;
+        return state;
       case ActionType.MOVE_CELL:
         const { direction } = action.payload;
         const index = state.order.findIndex((id) => id === action.payload.id);
         const targetIndex = direction === "up" ? index - 1 : index + 1;
 
         if (targetIndex < 0 || targetIndex > state.order.length - 1) {
-          return;
+          return state;
         }
 
         state.order[index] = state.order[targetIndex];
         state.order[targetIndex] = action.payload.id;
-        return;
+        return state;
       case ActionType.INSERT_CELL_BEFORE:
         const cell: Cell = {
           content: "",
@@ -61,13 +61,13 @@ const cellsReducer = produce(
           state.order.splice(foundIndex, 0, cell.id);
         }
 
-        return;
+        return state;
       case ActionType.BUNDLE_START:
-        return;
+        return state;
       case ActionType.BUNDLE_COMPLETE:
-        return;
+        return state;
       default:
-        return;
+        return state;
     }
   }
 );
