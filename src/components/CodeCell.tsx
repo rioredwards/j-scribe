bf51cf31d9a5e267e7feb1634b1d9daad32e361c
Rio Edwards <rioredwards@gmail.com>
2023-03-25 11:53:11 -0700

    Add show function which is global within user's code execution env. This makes it easy to display values in the preview

--- src/components/CodeCell.tsx
+++ src/components/CodeCell.tsx
@@ -18,7 +18,24 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const cumulativeCode = useTypedSelector((state) => {
     const { data, order } = state.cells;
     const orderedCells = order.map((id) => data[id]);
-    const cumulativeCode = [];
+
+    const cumulativeCode = [
+      `
+        const show = (value) => {
+          const root = document.querySelector('#root');
+
+          if (typeof value === 'object') {
+            if (value.$$typeof && value.props) {
+              ReactDOM.render(value, root);
+            } else {
+              root.innerHTML = JSON.stringify(value);
+            }
+          } else {
+            root.innerHTML = value;
+          }
+        };
+      `,
+    ];
     for (let c of orderedCells) {
       if (c.type === "code") {
         cumulativeCode.push(c.content);
