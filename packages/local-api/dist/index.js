d7f6e2bcd75177ed8a22fd17fcc9797bf31e620b
Rio Edwards <rioredwards@gmail.com>
2023-03-28 13:17:20 -0700

    Create link to local-client/build within local-api, which will be used to serve jbook app on user's computer

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -5,14 +5,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
 const express_1 = __importDefault(require("express"));
-const http_proxy_middleware_1 = require("http-proxy-middleware");
+const path_1 = __importDefault(require("path"));
 const serve = (port, filename, dir) => {
     const app = (0, express_1.default)();
-    app.use((0, http_proxy_middleware_1.createProxyMiddleware)({
-        target: "http://localhost:3000",
-        ws: true,
-        logLevel: "silent",
-    }));
+    const packagePath = require.resolve("local-client/build/index.html");
+    console.log("packagePath: ", packagePath);
+    const packageDir = path_1.default.dirname(packagePath);
+    console.log("packageDir: ", packageDir);
+    app.use(express_1.default.static(packageDir));
+    // app.use(
+    //   createProxyMiddleware({
+    //     target: "http://localhost:3000",
+    //     ws: true,
+    //     logLevel: "silent",
+    //   })
+    // );
     return new Promise((resolve, reject) => {
         app.listen(port, resolve).on("error", reject);
     });
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,16 +1,23 @@
 import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
+import path from "path";
 
 export const serve = (port: number, filename: string, dir: string) => {
   const app = express();
 
-  app.use(
-    createProxyMiddleware({
-      target: "http://localhost:3000",
-      ws: true,
-      logLevel: "silent",
-    })
-  );
+  const packagePath = require.resolve("local-client/build/index.html");
+  console.log("packagePath: ", packagePath);
+  const packageDir = path.dirname(packagePath);
+  console.log("packageDir: ", packageDir);
+  app.use(express.static(packageDir));
+
+  // app.use(
+  //   createProxyMiddleware({
+  //     target: "http://localhost:3000",
+  //     ws: true,
+  //     logLevel: "silent",
+  //   })
+  // );
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on("error", reject);
