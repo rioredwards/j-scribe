06adce7e002a69799fbdbd76deb562da2ea8e534
Rio Edwards <rioredwards@gmail.com>
2023-03-27 20:06:04 -0700

    Derive filepath/name in serve command

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,9 +1,14 @@
+import path from "path";
 import { Command } from "commander";
+import { serve } from "local-api";
 
 export const serveCommand = new Command()
   .command("serve [filename]")
   .description("Open a file for editing")
   .option("-p, --port <number>", "port to run server on", "4005")
-  .action((filename = "notebook.js", options) => {
-    console.log(filename, options);
+  .action((filename = "notebook.js", options: { port: string }) => {
+    const dir = path.join(process.cwd(), path.dirname(filename));
+    const file = path.basename(filename);
+
+    serve(parseInt(options.port), file, dir);
   });
--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -1,5 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = () => {
-    console.log("Server is listening");
+exports.serve = void 0;
+const serve = (port, filename, dir) => {
+    console.log("Serving traffic on port", port);
+    console.log("Saving/fetching cells from", filename);
+    console.log("That file is in dir", dir);
 };
+exports.serve = serve;
