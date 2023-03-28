ac07ee28d9c77473e3ed8a1ddcb134929e626a97
Rio Edwards <rioredwards@gmail.com>
2023-03-28 13:48:02 -0700

    Setup template for createCellsRouter

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
 import path from "path";
+import { createCellsRouter } from "./routes/cells";
 
 export const serve = (
   port: number,
@@ -25,6 +26,8 @@ export const serve = (
     console.log("packageDir: ", packageDir);
     app.use(express.static(packageDir));
   }
+
+  app.use(createCellsRouter(filename, dir));
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on("error", reject);
